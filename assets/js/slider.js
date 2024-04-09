88ea7ba184baec7059a18dbb89aa77ea7441fb0d
Maximiliano Safran <maxizero@gmail.com>
2024-04-08 23:51:59 -0300

    trying arrows

---
+++ assets/js/slider.js
@@ -0,0 +1,7 @@
+const tiles = document.getElementById("#tiles")
+const chevronLeft = document.querySelector(".fa-chevron-left")
+const chevronRight = document.querySelector(".fa-chevron-right")
+
+chevronLeft.addEventListener("click", () => {
+    // tiles.style.cssText = "transform: translate(-50%)"
+})
